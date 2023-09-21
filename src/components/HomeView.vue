9882352ef619ddf4365e1487e8dcf3a1530e951f
Kyle Henning <kylebhenning@gmail.com>
2023-09-20 21:39:56 -0700

    added operators

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -17,22 +17,28 @@ import { ref } from 'vue';
   }
 
   const changeHex = () => {
+    let num1 = parseInt(color1.value, 16)
+    let num2 = parseInt(color2.value, 16)
+    let solution;
     switch (operator.value) {
       case 'multiply':
-        console.log("color 1", color1.value);
-        console.log('color 2', color2.value)
-        let num1 = parseInt(color1.value, 16)
-        let num2 = parseInt(color2.value, 16)
-        console.log("num 1", num1)
-        console.log("num2", num2)
-        let product = num1 * num2;
-        let fakeMod = parseInt('FFFFFF', 16);
-
-        product = product % fakeMod;
-        console.log("product: ", product)
-        color3.value = product.toString(16);
+        solution = num1 * num2;
+        break;
+      case 'add':
+        solution = num1 + num2;
+        break;
+      case 'subtract':
+        solution = num1 - num2;
+        break;
+      case 'divide':
+        solution = num1 / num2;
+        break;
     }
-    console.log("color3: ", color3.value)
+    let fakeMod = parseInt('FFFFFF', 16);
+
+    solution = solution % fakeMod;
+    color3.value = solution.toString(16);
+    console.log("Kyle's solution: ", color3.value)
   }
 
   const juliasWay = () => {
@@ -41,12 +47,24 @@ import { ref } from 'vue';
     let productsArray = [];
     let final= "";
     for (var i = 0; i < 3; i++) {
-      productsArray.push((parseInt(hex1Array[i], 16) * parseInt(hex2Array[i], 16)) % 255);
+      switch (operator.value) {
+      case 'multiply':
+        productsArray.push((parseInt(hex1Array[i], 16) * parseInt(hex2Array[i], 16)) % 255);
+        break;
+      case 'add':
+        productsArray.push((parseInt(hex1Array[i], 16) + parseInt(hex2Array[i], 16)) % 255);
+        break;
+      case 'subtract':
+        productsArray.push((parseInt(hex1Array[i], 16) - parseInt(hex2Array[i], 16)) % 255);
+        break;
+      case 'divide':
+        productsArray.push((parseInt(hex1Array[i], 16) / parseInt(hex2Array[i], 16)) % 255);
+        break
+    }
       productsArray[i] = productsArray[i].toString(16);
       final = final + productsArray[i];
     }
-    console.log("product array: ", productsArray);
-    console.log("here is final", final)
+    console.log("Julia's solution: ", final)
     color4.value = final;
 
   }
@@ -65,13 +83,23 @@ import { ref } from 'vue';
       <input type="text" @input="handleInput2" class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/3 p-2.5" :value="color2" />
       <div :style="{'background-color': '#' + color2}" class=" w-1/4 border border-black" ></div>
     </div>
+    <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
+    <select id="countries" v-model="operator" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5">
+      <option selected>Choose an operation</option>
+      <option value="multiply">Multiplication</option>
+      <option value="add">Addition</option>
+      <option value="subtract">Subtract</option>
+      <option value="divide">Divide</option>
+    </select>
     <div class="flex flex-row w-1/2 m-20 justify-between">
       <button @click="changeHex" class="bg-blue-400 w-24 p-5 rounded">Try it</button>
-      <div :style="{'background-color': '#' + color3}" class=" w-1/4 border border-black" >PRODUCT</div>
+      <div :style="{'background-color': '#' + color3}" class=" w-1/4 border border-black" ></div>
     </div>
     <div class="flex flex-row w-1/2 m-20 justify-between">
       <button @click="juliasWay" class="bg-blue-400 w-24 p-5 rounded">JULIAS WAY</button>
-      <div :style="{'background-color': '#' + color4}" class=" w-1/4 border border-black" >PRODUCT</div>
+      <div :style="{'background-color': '#' + color4}" class=" w-1/4 border border-black" ></div>
     </div>
+    
+
   </div>
 </template>
